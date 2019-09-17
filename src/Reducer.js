01ce686ec558f263751536fef0be9042578a64e8
Samih Munshi <samih.munshi@level.global>
2019-09-17 22:40:41 +0100

    Added user settings reducer for future user defined settings feature.

--- src/Reducer.js
+++ src/Reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import chartData from "./reducers/ChartDataReducer";
 import tradeData from "./reducers/ExecuteTradeReducer";
 import sessionData from "./reducers/SessionReducer";
+import settingsData from "./reducers/SettingsReducer";
 export default combineReducers({
     chartData,
     tradeData,
-    sessionData
+    sessionData,
+    settingsData
 });
 
---
+++ src/reducers/SettingsReducer.js
@@ -0,0 +1,21 @@
+export default (state = { chartIntervals: ['4'], pairs: ['USD'], symbols: ['BTC'] }, action) => {
+    switch (action.type) {
+        case 'SET_INTERVAL':
+            return {
+                ...state,
+                chartIntervals: action.payload.chartIntervals
+            };
+        case 'SET_PAIRS':
+            return {
+                ...state,
+                pairs: action.payload.pairs
+            };
+        case 'SET_SYMBOLS':
+            return {
+                ...state,
+                symbols: action.payload.symbols
+            }
+        default:
+                return state;
+    }
+}
